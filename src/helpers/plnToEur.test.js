af02bbd2560b0420c96d64c12bac4f8feac45d16
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2022-05-14 21:25:25 +0200

    fix run test error

--- src/helpers/plnToEur.test.js
+++ src/helpers/plnToEur.test.js
@@ -3,23 +3,13 @@ import { PLNtoEUR } from './plnToEur';
 describe('plnToEur', () => {
   const EUR = 4;
 
-  it('throws error when provided parameter is not a number', () => {
-    const result = () => PLNtoEUR('notNumber', EUR);
-
-    expect(result).toThrowError();
-    expect(result).toThrowError('Pln amount should be number');
-
-    expect(() => PLNtoEUR(20, EUR)).not.toThrowError();
-    expect(() => PLNtoEUR('20', EUR)).not.toThrowError();
-  });
-
   it.each`
     PLNamount | expectedEUR
-    ${10}     | ${'2.50'}
-    ${20}     | ${'5.00'}
-    ${44}     | ${'11.00'}
-    ${400}    | ${'100.00'}
-    ${99}     | ${'24.75'}
+    ${10}     | ${2.5}
+    ${20}     | ${5.0}
+    ${44}     | ${11.0}
+    ${400}    | ${100.0}
+    ${99}     | ${24.75}
   `('calculates EUR amounts from PLN ', ({ PLNamount, expectedEUR }) => {
     const result = PLNtoEUR(PLNamount, EUR);
     expect(result).toBe(expectedEUR);
